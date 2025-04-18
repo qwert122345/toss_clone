79350956f0b4dcc10c66e03fc10f67944222cb24
고승환 <hwannnnnnnnnnnn@goseunghwan-ui-MacBookAir.local>
2025-04-18 12:41:15 +0900

    .

--- app/src/main/java/com/example/tossclonedesign/MainActivity.kt
+++ app/src/main/java/com/example/tossclonedesign/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.tossclonedesign
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val Transfer = findViewById<LinearLayout>(R.id.transfer_btn)
+        Transfer.setOnClickListener{
+            val pageEvent = Intent(this,MainActivity::class.java)
+            startActivity(pageEvent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/tossclonedesign/send.kt
+++ app/src/main/java/com/example/tossclonedesign/send.kt
@@ -10,7 +10,7 @@ class send : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_send)
+        setContentView(R.layout.activity_account)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
